f1e8cdbb22cedaa36c68f71f64851ed4b58b2d1e
Charles Bruno <charlesbrcosta@gmail.com>
2024-03-21 16:03:10 -0300

    conclusão do style.css e adicionado a logica do javascript. foram feitos ajuste no index.html e no style.css

---
+++ app.js
@@ -0,0 +1,38 @@
+let btnNext = document.getElementById('next');
+let btnPrev = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+btnNext.onclick = () => {
+    showSlider('next');
+}
+
+btnPrev.onclick = () => {
+    showSlider('prev');
+}
+
+let timRunning = 3000;
+let runTimeOut;
+
+function showSlider(type){
+    let itemSlider = document.querySelectorAll('.carousel .list .item');
+    let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+
+    if(type === 'next'){
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carouselDom.classList.add('next');
+    } else {
+        let positionLastItem = itemSlider.length - 1;
+        listItemDom.prepend(itemSlider[positionLastItem]);
+        thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+        carouselDom.classList.add('prev');
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timRunning);
+}
