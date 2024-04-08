1158a5a89f8fdf2a517a9eb01d91648f30834702
Charles Bruno <charlesbrcosta@gmail.com>
2024-04-08 12:34:45 -0300

    Adiconado README, comentários na lógica app e video demostrativo

---
+++ README.md
@@ -0,0 +1,40 @@
+## Carrossel Imagens interativa
+
+Este é um projeto de carrossel desenvolvido utilizando HTML, CSS e JavaScript, inspirado em um tutorial criado por `**Luv Dev**` no YouTube. O objetivo deste projeto foi aprender e praticar a implementação de efeitos de carrossel em páginas da web.
+
+## :dizzy: Tecnologias utilizadas
+
+<div>
+  <img src="https://img.shields.io/badge/HTML5-e34c26?style=for-the-badge&logo=html5&logoColor=white">
+  <img src="https://img.shields.io/badge/CSS3-264de4?style=for-the-badge&logo=css3&logoColor=white">
+  <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">
+</div>
+
+## :hammer: Funcionalidades
+
+- `Carrossel de Imagens e Informações:` O projeto inclui um carrossel que exibe imagens de diferentes animais, juntamente com informações sobre eles, como autor, título, tópico e uma descrição detalhada.
+
+- `Botões de Navegação:` Os botões de navegação "Próximo" e "Anterior" permitem aos usuários avançar ou retroceder nas imagens e informações apresentadas no carrossel.
+
+- `Efeito de Transição:` Ao clicar nos botões de navegação, é aplicado um efeito de transição suave entre as imagens, proporcionando uma experiência visual agradável.
+
+- `Miniaturas das Imagens:` Abaixo do carrossel principal, há uma seção de miniaturas das imagens, permitindo uma visualização rápida e fácil de todos os animais apresentados.
+
+## :mag: Demonstração
+
+Para uma experiência completa e visual do projeto em funcionamento, assista ao vídeo demonstrativo abaixo:
+
+![Demonstração](./videoDemo/projeto.mp4)
+
+Além disso, você pode acessar a demonstração interativa do projeto, [clique aqui.](https://charlesbrcosta.github.io/CarouselImagensInterativo/)
+
+
+## Como Utilizar
+
+1. Clone o repositório em seu ambiente de desenvolvimento.
+2. Abra o arquivo index.html em seu navegador preferido.
+3. Utilize os botões de navegação "Próximo" e "Anterior" para explorar as diferentes imagens e informações no carrossel.
+
+## Autor
+
+Este projeto foi desenvolvido, com base no tutorial criado por Luv Dev no YouTube, como parte do aprendizado e prática de efeitos de carrossel em desenvolvimento web.
--- app.js
+++ app.js
@@ -1,36 +1,50 @@
+// Selecionando os elementos do carrossel e os botões de navegação
 let btnNext = document.getElementById('next');
 let btnPrev = document.getElementById('prev');
 let carouselDom = document.querySelector('.carousel');
 let listItemDom = document.querySelector('.carousel .list');
 let thumbnailDom = document.querySelector('.carousel .thumbnail');
 
+// Ação ao clicar no botão "Próximo"
 btnNext.onclick = () => {
-    showSlider('next');
+    showSlider('next'); // Chama a função showSlider com o argumento 'next'
 }
 
+// Ação ao clicar no botão "Anterior"
 btnPrev.onclick = () => {
     showSlider('prev');
 }
 
-let timRunning = 3000;
+// Definindo o tempo de execução do carrossel e uma variável para o timeout
+let timRunning = 3000; // Tempo em milissegundos (3 segundos)
 let runTimeOut;
 
+// Função para mostrar o próximo ou o anterior slide
 function showSlider(type){
+    // Selecionando todos os itens do carrossel e suas miniaturas
     let itemSlider = document.querySelectorAll('.carousel .list .item');
     let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
 
+    // Verifica se o tipo de ação é 'next' (próximo) ou 'prev' (anterior)
     if(type === 'next'){
+        // Move o primeiro item para o final da lista no carrossel e nas miniaturas
         listItemDom.appendChild(itemSlider[0]);
         thumbnailDom.appendChild(itemThumbnail[0]);
+        // Adiciona a classe 'next' à div do carrossel para aplicar o efeito de transição
         carouselDom.classList.add('next');
     } else {
+        // Calcula a posição do último item na lista
         let positionLastItem = itemSlider.length - 1;
         listItemDom.prepend(itemSlider[positionLastItem]);
         thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+        // Adiciona a classe 'prev' à div do carrossel para aplicar o efeito de transição
         carouselDom.classList.add('prev');
     }
 
+    // Limpa o timeout anterior para evitar múltiplas execuções simultâneas
     clearTimeout(runTimeOut);
+
+    // Define um novo timeout para remover as classes de transição após o tempo especificado
     runTimeOut = setTimeout(() => {
         carouselDom.classList.remove('next');
         carouselDom.classList.remove('prev');
